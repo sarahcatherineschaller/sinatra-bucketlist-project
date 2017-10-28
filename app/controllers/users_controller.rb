c08066eccd6f6c4cfd1f069ba54d7a889f1911b3
sarahschaller <sarcsch@aol.com>
2017-10-28 14:17:48 -0400

    fix sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
 		if params[:username] == "" || params[:email] == "" || params[:password] == ""
 			redirect '/signup'
 		else 
-			user = User.create(username: params[:username], email: params[:email], password: params[:password])
+			@user = User.create(username: params[:username], email: params[:email], password: params[:password])
+			@user.save
 			session[:user_id] = user.id 
 			redirect '/bucketlist'
 		end 
