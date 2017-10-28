ebe9792a44b622a915fd5d50515fb485c927d2c0
sarahschaller <sarcsch@aol.com>
2017-10-28 14:12:40 -0400

    add items controller actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,29 @@
 class ItemsController < ApplicationController 
+
+	get '/bucketlist' do 
+		if !logged_in? 
+			redirect '/login' 
+		else 
+			@user = current_user 
+			@items = Item.all
+			erb :'/items/bucketlist'
+		end 
+	end 
+
+	get '/bucketlist/new' do 
+		if !logged_in?
+			redirect '/login' 
+		else 
+			erb :'/items/create_item'
+		end 
+	end 
+
+	post '/bucketlist' do 
+		if params[:item] == "" 
+			redirect to '/bucketlist/new'
+		else 
+			@item = current_user.bucketlist.create(item: params[:item], notes: params[:notes])
+			redirect "/bucketlist/#{@item.id}"
+		end 
+	end
 end
