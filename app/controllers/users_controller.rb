c8cdd1d6f605aa23702a412c2b57db4256b152d0
sarahschaller <sarcsch@aol.com>
2017-10-28 13:53:18 -0400

    update users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController 
+
+	get '/signup' do 
+		if logged_in?
+			redirect '/bucketlist'
+		else 
+			erb :'users/signup'
+		end 
+	end
+
+	post '/signup' do 
+		if params[:username] == "" || params[:email] == "" || params[:password] == ""
+			redirect '/signup'
+		else 
+			user = User.create(username: params[:username], email: params[:email], password: params[:password])
+			session[:user_id] = user.id 
+			redirect '/bucketlist'
+		end 
+	end
+
+	get '/login' do 
+		if logged_in?
+			redirect '/bucketlist'
+		else 
+			erb :'users/login'
+		end 
+	end
+
+	post '/login' do 
+		user = User.find_by(username: params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect '/bucketlist'
+		else
+			redirect '/signup'
+		end 
+	end
+
+end
