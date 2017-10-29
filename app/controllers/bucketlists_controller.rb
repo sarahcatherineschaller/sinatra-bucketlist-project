c64ce2a1361521721aa1c8cc4210185673d2a31c
sarahschaller <sarcsch@aol.com>
2017-10-29 08:43:27 -0400

    add bucketlist controller actions

--- app/controllers/bucketlists_controller.rb
+++ app/controllers/bucketlists_controller.rb
@@ -1,6 +1,30 @@
 class BucketlistsController < ApplicationController 
 
-	
+	get '/bucketlist' do 
+		if !logged_in?
+			redirect '/login' 
+		else 
+			@bucketlist = Bucketlist.all 
+			erb :'/bucketlist/bucketlist'
+		end 
+	end
+
+	get '/bucketlist/new' do 
+		if !logged_in?
+			redirect '/login' 
+		else 
+			erb :'/bucketlist/create_item' 
+		end 
+	end 
+
+	post '/bucketlist' 
+  	    if params[:item] == ""
+		  redirect '/bucketlist/new'
+	    else 
+		  @bucketlist_item = current_user.bucketlist.create(item: params[:item])
+		  redirect "/bucketlist/#{@bucketlist_item.id}"
+	    end 
+	end
 
 
 
